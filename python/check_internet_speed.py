0b8fcf3366e0855055b8fc37d21010e042defcbe
Jack Allen <jackallensemail+git@gmail.com>
2019-11-17 18:50:01 +0000

    db

--- python/check_internet_speed.py
+++ python/check_internet_speed.py
@@ -1,10 +1,20 @@
 import subprocess
 import json
+from requests import post
 
 print('Performing check...')
 
 output = subprocess.check_output(['speedtest-cli', '--json'])
 
 results = json.loads(output)
+
+
+print('Response received')
+print('Sending to server...')
+
+response = post('http://178.62.32.208/dump', results)
+
+print('Server responded with:')
+print(response.json())
 
 print(results['client'])
