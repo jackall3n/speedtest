845f63ef3563974e876599025e180008ca2965fc
Jack Allen <jackallensemail+git@gmail.com>
2019-11-17 22:11:58 +0000

    cors

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import fs from 'fs-extra';
 import path from 'path';
+import cors from 'cors';
 import Database from "./database";
 
 const { PORT = 3000, OUTPUT = './db' } = process.env;
@@ -9,6 +10,7 @@ let database: Database;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
